8abaa40ba190b523d69428b1489fb439a6faa797
Dasun Piyumal <w.piyumal2319@gmail.com>
2025-08-17 09:20:30 +0530

    Add constructors to CategoryProjectionDTO for Jackson compatibility and update project version to 1.0.3-SNAPSHOT

--- src/main/java/dto/projection/CategoryProjectionDTO.java
+++ src/main/java/dto/projection/CategoryProjectionDTO.java
@@ -1,12 +1,16 @@
 package dto.projection;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
 @Builder
+@NoArgsConstructor      // ✅ ADDED: Jackson needs this to create the object
+@AllArgsConstructor
 public class CategoryProjectionDTO {
     private UUID id;
     private String name;
