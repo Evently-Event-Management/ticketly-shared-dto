73fbab807e795d3e3b5158a8485ed59d592ea72b
Dasun Piyumal <w.piyumal2319@gmail.com>
2025-09-22 09:33:15 +0530

    Enhance DiscountProjectionDTO with Lombok annotations and update project version to 1.1.5-SNAPSHOT

--- src/main/java/dto/projection/DiscountProjectionDTO.java
+++ src/main/java/dto/projection/DiscountProjectionDTO.java
@@ -1,11 +1,19 @@
 package dto.projection;
 
 import dto.projection.discount.DiscountParametersProjectionDTO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.UUID;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class DiscountProjectionDTO {
     private UUID id;
     private String code;
