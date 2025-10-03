1ca935ccac67588a7e864f4563e453cd8e29f938
Dasun Piyumal <w.piyumal2319@gmail.com>
2025-10-03 19:04:43 +0530

    Update DiscountProjectionDTO to use TierInfo and increment version to 1.1.9-SNAPSHOT

---
+++ src/main/java/dto/CreateOrderRequest.java
@@ -0,0 +1,29 @@
+package dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Request DTO sent from the Order Service to the Query Service
+ * to check if a list of seats are available for a specific session.
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateOrderRequest {
+    @NotNull
+    private UUID event_id;
+    @NotNull
+    private UUID session_id;
+    @NotEmpty
+    private List<String> seat_ids;
+    private String discount_id;
+}
